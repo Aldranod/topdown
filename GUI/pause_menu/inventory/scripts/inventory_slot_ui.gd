57928e41a0aeeb4d452ecc0192fd24c38c7faf59
Aldranod <kurzydlo.b@gmail.com>
2025-09-07 11:11:56 +0200

    inventory system

--- GUI/pause_menu/inventory/scripts/inventory_slot_ui.gd
+++ GUI/pause_menu/inventory/scripts/inventory_slot_ui.gd
@@ -15,9 +15,15 @@ func _ready() -> void:
 func set_slot_data( value : SlotData ) -> void:
 	slot_data = value
 	if slot_data == null:
+		texture_rect.texture = null
+		label.text = ""
 		return
+		
 	texture_rect.texture = slot_data.item_data.texture
-	label.text = str( slot_data.quantity)	
+	if slot_data.item_data is EqupableItemData:
+		label.text = ""
+	else:	
+		label.text = str( slot_data.quantity)	
 	
 func item_focused() -> void:
 	if slot_data != null:
@@ -37,7 +43,10 @@ func item_pressed()	-> void:
 			if was_used == false:
 				return
 			slot_data.quantity -= 1
-			label.text = str(slot_data.quantity)	
+			if slot_data == null:
+				return
+			else:	
+				label.text = str(slot_data.quantity)	
 	pass
 		
 	
