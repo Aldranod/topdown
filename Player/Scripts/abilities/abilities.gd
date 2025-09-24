d712b2167552af37627568359249286b5ff84734
Aldranod <kurzydlo.b@gmail.com>
2025-09-24 21:52:26 +0200

    grapple hook

--- Player/Scripts/abilities/abilities.gd
+++ Player/Scripts/abilities/abilities.gd
@@ -15,6 +15,7 @@ var boomerang_instance : Boomerang = null
 @onready var idle: State_Idle = $"../StateMachine/Idle"
 @onready var walk: State_Walk = $"../StateMachine/Walk"
 @onready var bow: State_Bow = $"../StateMachine/Bow"
+@onready var grapple: State_Grapple = $"../StateMachine/Grapple"
 
 
 func _ready() -> void:
@@ -28,7 +29,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			0:
 				boomerang_ability()
 			1:
-				print("grapple hook!")	
+				grapple_ability()
 			2:
 				bow_ability()
 			3:
@@ -75,4 +76,9 @@ func bow_ability() -> void:
 	elif state_machine.current_state == idle or state_machine.current_state == walk:
 		player.arrow_count -= 1
 		player.state_machine.change_state(bow)
+	pass
+
+func grapple_ability() ->void:
+	if state_machine.current_state == idle or state_machine.current_state == walk:
+		player.state_machine.change_state(grapple)
 	pass
