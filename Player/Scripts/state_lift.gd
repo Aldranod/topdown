25550de34665bdc3cf413bbff5d8ef7119e46f9c
Aldranod <kurzydlo.b@gmail.com>
2025-09-24 13:15:44 +0200

    bomb

--- Player/Scripts/state_lift.gd
+++ Player/Scripts/state_lift.gd
@@ -4,6 +4,7 @@ class_name State_Lift extends State
 
 @onready var carry: State = $"../Carry"
 
+var start_anim_late : bool = false
 
 func _ready():
 	pass
@@ -13,12 +14,15 @@ func init() -> void:
 	
 func Enter() -> void:
 	player.UpdateAnimation("lift")
+	if start_anim_late == true:
+		player.animation_player.seek( 0.2)
 	player.animation_player.animation_finished.connect( state_complete)
 	player.audio.stream = lift_audio
 	player.audio.play()
 	pass
 	
 func Exit() -> void:
+	start_anim_late = false
 	pass
 	
 func Process(_delta: float) -> State:
