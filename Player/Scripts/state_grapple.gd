35ef832770e71da030278c3b9b89d8659175c86e
Aldranod <kurzydlo.b@gmail.com>
2025-09-25 09:34:30 +0200

    grapple hook

--- Player/Scripts/state_grapple.gd
+++ Player/Scripts/state_grapple.gd
@@ -36,22 +36,130 @@ var pos_map : Dictionary = {
 
 func init() -> void:
 	grapple_hook.visible = false
-	#grapple_ray_cast_2d.enabled = false
+	grapple_ray_cast_2d.enabled = false
 	grapple_ray_cast_2d.target_position.y = grapple_distance
 	pass
 
 func Enter() -> void:
-	print("grapple state!")
+	player.UpdateAnimation("idle")
+	grapple_hook.visible = true
+	set_grapple_position()
+	raycast_detection()
+	shoot_grapple()
+	chain_audio_player.play()
+	play_audio( grapple_fire_audio)
 	pass
 	
 func Exit() -> void:
+	next_state = null
+	grapple_hook.visible = false
+	chain_audio_player.stop()
+	tween.kill()
+	nine_patch_rect.size.y = nine_patch_size
 	pass
 	
 func Process(_delta: float) -> State:
-	return null
+	player.velocity = Vector2.ZERO
+	return next_state
 
 func Physics(_delta: float) -> State:
 	return null	
 
 func HandleInput( _event: InputEvent) -> State:
-	return null						
+	return null			
+	
+func set_grapple_position() -> void:
+	var new_pos : Vector3 = positions[
+		pos_map[player.cardinal_direction]
+	]
+	grapple_hook.position = Vector2(new_pos.x, new_pos.y)
+	grapple_hook.rotation_degrees = new_pos.z
+	if player.cardinal_direction == Vector2.UP:
+		grapple_hook.show_behind_parent = true
+	else:
+		grapple_hook.show_behind_parent = false	
+	pass				
+
+func raycast_detection() -> void:
+	collision_type = 0
+	collision_distance = grapple_distance
+	grapple_ray_cast_2d.set_collision_mask_value(5, false)
+	grapple_ray_cast_2d.set_collision_mask_value(6, true)
+	grapple_ray_cast_2d.force_raycast_update()
+	if grapple_ray_cast_2d.is_colliding():
+		collision_type = 2
+		collision_distance = grapple_ray_cast_2d.get_collision_point().distance_to(player.global_position)
+		return	
+	grapple_ray_cast_2d.set_collision_mask_value(5, true)
+	grapple_ray_cast_2d.set_collision_mask_value(6, false)
+	grapple_ray_cast_2d.force_raycast_update()
+	if grapple_ray_cast_2d.is_colliding():
+		collision_type = 1
+		collision_distance = grapple_ray_cast_2d.get_collision_point().distance_to(player.global_position)
+		return	
+	pass
+
+func shoot_grapple() -> void:
+	if tween:
+		tween.kill()
+	var tween_duration : float = collision_distance	/ grapple_speed
+	tween = create_tween()
+	tween.tween_property(
+		nine_patch_rect, "size",
+		Vector2(nine_patch_rect.size.x, collision_distance),
+		tween_duration
+	)
+	if collision_type == 2:
+		tween.tween_callback( grapple_player)
+	else:
+		tween.tween_callback( return_grapple)	
+	pass
+
+func grapple_finished() -> void:
+	player.set_collision_mask_value(4, true)
+	next_state = idle
+	pass
+
+func play_audio( audio : AudioStream) -> void:
+	player.audio.stream = audio
+	player.audio.play()
+	pass
+
+func grapple_player() -> void:
+	if tween:
+		tween.kill()
+	play_audio(grapple_stick_audio)
+	player.set_collision_mask_value(4, false)	
+	var tween_duration : float = collision_distance	/ grapple_speed
+	tween = create_tween()
+	tween.tween_property(
+		nine_patch_rect, "size",
+		Vector2(nine_patch_rect.size.x, nine_patch_size),
+		tween_duration
+	)
+	var player_target : Vector2 = player.global_position  
+	player_target += (player.cardinal_direction * collision_distance)
+	player_target -= player.cardinal_direction * 10
+	tween.parallel().tween_property(
+		player, "global_position",
+		player_target,
+		tween_duration
+	)
+	player.make_invulnerable(tween_duration)
+	tween.tween_callback(grapple_finished)
+	pass
+	
+func return_grapple() -> void:
+	if tween:
+		tween.kill()	
+	if collision_type > 0:
+		play_audio(grapple_bounce_audio)	
+	var tween_duration : float = collision_distance	/ grapple_speed
+	tween = create_tween()
+	tween.tween_property(
+		nine_patch_rect, "size",
+		Vector2(nine_patch_rect.size.x, nine_patch_size),
+		tween_duration
+	)
+	tween.tween_callback(grapple_finished)
+	pass	
